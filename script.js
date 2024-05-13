c9c7c70da8e9c03b2a555789b94d232578f57776
Tobi <tobiawolaju21@gmail.com>
2024-05-13 02:41:31 +0100

    bm,jbkn

---
+++ script.js
@@ -0,0 +1,8 @@
+var viewportHeight = window.innerHeight/3;
+
+window.addEventListener('scroll', function() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var newOpacity = 1 - (scrollTop / viewportHeight); // Adjust the value 200 to control when the opacity starts to fade
+    newOpacity = Math.min(1, Math.max(0, newOpacity)); // Ensure opacity stays between 0 and 1
+    document.querySelector('.headies').style.opacity = newOpacity;
+});
