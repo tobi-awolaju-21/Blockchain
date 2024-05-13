7b2d8586fd2168abc41053d5366102b2cf8ef419
Tobi <tobiawolaju21@gmail.com>
2024-05-13 09:15:49 +0100

    m,jkn.

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ window.addEventListener('scroll', function() {
     newOpacity = Math.min(1, Math.max(0, newOpacity)); // Ensure opacity stays between 0 and 1
     document.querySelector('.headies').style.opacity = newOpacity;
 });
+
+function redirectToGitHub() {
+    // Define the URL you want to redirect to
+    var url = 'https://github.com/tobi-awolaju-21';
+    
+    // Open the URL in a new tab
+    window.open(url, '_blank');
+}
